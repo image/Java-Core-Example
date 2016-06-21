cd28d6dbeaee92cc659c4441187da43bc79af73d
image <ymy2013.4@gmail.com>
2016-06-22 00:15:34 +0800

    java project

---
+++ src/main/java/com/selrain/Java_Core_Example/App.java
@@ -0,0 +1,13 @@
+package com.selrain.Java_Core_Example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
